37cc2e3468372bd4f7b4385aa2a4a4a0ddd1810c
Vantz Stockwell <vantz.stockwell@gmail.com>
2021-03-13 02:28:27 -0500

    Update hypervhost-boxstarter.ps1

--- Hyper-V Host Boxstarter/hypervhost-boxstarter.ps1
+++ Hyper-V Host Boxstarter/hypervhost-boxstarter.ps1
@@ -2,9 +2,6 @@
 # Apex Technology
 # 3/13/2021 Vantz Stockwell
 
-# Install Chocolatey Package Manager
-
-
 $Boxstarter.RebootOk=$true # Allow reboots?
 $Boxstarter.NoPassword=$false # Is this a machine with no login password?
 $Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
