d79842415453d3924a7571df4fa77cac565101f3
Vantz Stockwell <vantz.stockwell@gmail.com>
2021-03-13 04:11:44 -0500

    Update hypervhost-boxstarter.ps1
    Updated Script

--- Hyper-V Host Boxstarter/hypervhost-boxstarter.ps1
+++ Hyper-V Host Boxstarter/hypervhost-boxstarter.ps1
@@ -7,7 +7,6 @@ $Boxstarter.NoPassword=$false # Is this a machine with no login password?
 $Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
 
 # Basic Windows Settings
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
 Enable-RemoteDesktop
 Update-ExecutionPolicy RemoteSigned
 Disable-InternetExplorerESC
@@ -38,3 +37,49 @@ Install-PackageProvider -Name NuGet -Force
 Install-Script -Name Test-PendingReboot -Force
 
 if (Test-PendingReboot) { Invoke-Reboot }
+
+# Boxstarter Requirements
+cinst boxstarter.common -y --force
+cinst boxstarter.winconfig -y --force
+cinst boxstarter.Bootstrapper -y --force
+cinst boxstarter.Chocolatey -y --force
+cinst boxstarter.HyperV -y --force
+
+# Install Hyper-V Role
+Install-WindowsFeature -name hyper-v -IncludeManagementTools
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+# Install Basic Server Software
+cinst firefoxesr -y
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+# Windows Updates
+Install-WindowsUpdate -AcceptEula
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+# Rename Computer and Set Timezon
+#--- Rename the Computer ---
+$computername = "LI-HyperV01"
+if ($env:computername -ne $computername) {
+	Rename-Computer -NewName $computername
+}
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+Rename-NetAdapter -Name "NIC1" -NewName "Converged Local Area Network Connection 1"
+Rename-NetAdapter -Name "NIC2" -NewName "Converged Local Area Network Connection 2"
+New-NetLbfoTeam -Name "Converged Local Area Network Connection Team" -TeamMembers "Converged Local Area Network Connection 1","Converged Local Area Network Connection 2" -y
+Get-NetAdapter teama | Set-DnsClientServerAddress -ServerAddresses New-VMSwitch -name "Converged Local Area Network Connection Team - Virtual Network" -NetAdapterName "Converged Local Area Network Connection Team" -AllowManagementOs $true
+Get-NetAdapter teama | Set-DnsClientServerAddress -ServerAddresses '1.1.1.1','1.0.0.1'
+New-Item -Path D:\ -Name Hyper-V -ItemType Directory
+Set-VMHost -VirtualMachinePath 'D:\Hyper-V'
+New-Item -Path C:\ -Name Temp -ItemType Directory	
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://carolinashost.com/Systems-Management_Application_8CTK7_WN64_1.9.0_A00.EXE","C:\temp\Systems-Management_Application_8CTK7_WN64_1.9.0_A00.EXE")
+$pathvargs = {C:\temp\Systems-Management_Application_8CTK7_WN64_1.9.0_A00.EXE /S /v/qn }
+Invoke-Command -ScriptBlock $pathvargs
+Invoke-Reboot
+& 'C:\Program Files\Dell\DELL EMC system Update\DSU' @('--apply-upgrades', '--non-interactive', '--reboot')
